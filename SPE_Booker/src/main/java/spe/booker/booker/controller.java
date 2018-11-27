f90a360a8a3d8d24851a6a3b417023ced69d2d80
aidan <ah16165@bristol.ac.uk>
2018-11-27 10:37:04 +0000

    working html

--- SPE_Booker/src/main/java/spe/booker/booker/controller.java
+++ SPE_Booker/src/main/java/spe/booker/booker/controller.java
@@ -1,21 +1,17 @@
 package spe.booker.booker;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-
-@RestController
+@Controller
 public class controller {
 
-    @RequestMapping("/")
-    public String index() {
-        return "Test";
-    }
-
-
-    public static void main(String[] args) {
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 
 }

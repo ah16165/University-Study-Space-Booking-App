daddcee8a3c683f4f453c4945bc59f105d0c8458
aidan <ah16165@bristol.ac.uk>
2018-12-02 17:14:26 +0000

    Commented out all the PostgresSQL stuff cos it broke lol

--- src/main/java/spe/booker/booker/DatabaseConfig.java
+++ src/main/java/spe/booker/booker/DatabaseConfig.java
@@ -1,20 +1,23 @@
 package spe.booker.booker;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-
-import javax.sql.DataSource;
-
-@Configuration
-public class DatabaseConfig {
-    @Bean
-    @Primary
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource dataSource() {
-        return DataSourceBuilder.create().build();
-    }
-}
-
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.boot.jdbc.DataSourceBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
+//
+//import javax.sql.DataSource;
+//
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+//@Configuration
+//public class DatabaseConfig {
+//    @Bean
+//    @Primary
+//    @ConfigurationProperties(prefix = "spring.datasource")
+//    public DataSource dataSource() {
+//        return DataSourceBuilder.create().build();
+//    }
+//}
+//

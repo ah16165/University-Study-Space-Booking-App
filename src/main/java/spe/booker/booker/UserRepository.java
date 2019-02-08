efd7f3265b7e1a7ac14736365c0da01fb6314f8b
Unknown <aidanhood@btinternet.com>
2019-02-08 13:05:16 +0000

    finish for the day

--- src/main/java/spe/booker/booker/UserRepository.java
+++ src/main/java/spe/booker/booker/UserRepository.java
@@ -1,12 +1,13 @@
 package spe.booker.booker;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
 
-import spe.booker.booker.User;
+
+import org.springframework.data.repository.CrudRepository;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
-
+@Component
 public interface UserRepository extends CrudRepository<User, Integer> {
 
 }

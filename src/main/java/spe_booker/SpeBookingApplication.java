17a5f22266ec6decd6d378689468f39e62c52352
Unknown <ah16165@bristol.ac.uk>
2019-02-10 18:58:16 +0000

    Completed main class. Changed about yaml files. Restructured entire project.

---
+++ src/main/java/spe_booker/SpeBookingApplication.java
@@ -0,0 +1,21 @@
+package spe_booker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@PropertySources({
+        @PropertySource("classpath:application.properties"),
+        @PropertySource(value = "file:$/home/booker/.secret.properties", ignoreResourceNotFound = true)
+})
+
+@SpringBootApplication
+public class SpeBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeBookingApplication.class, args);
+    }
+
+}
+

fb314e42b0df4f6fb738900e5cda9ca3575a036f
Unknown <aidanhood@btinternet.com>
2019-02-15 12:06:49 +0000

    witched back

--- src/main/java/spe_booker/models/Slot.java
+++ src/main/java/spe_booker/models/Slot.java
@@ -27,7 +27,7 @@ public class Slot {
     private java.util.Date dateTime;
 
     @NotNull
-    @OneToMany(cascade = CascadeType.ALL)
+    @ManyToOne(cascade = CascadeType.ALL)
     private Room room;
 
     @OneToMany(mappedBy = "slot", cascade = CascadeType.ALL)

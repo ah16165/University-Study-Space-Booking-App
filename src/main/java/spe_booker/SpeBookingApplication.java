b1886a26cae3e1682aaa26464daa5791f64f44f0
Unknown <aidanhood@btinternet.com>
2019-02-15 11:48:35 +0000

    edited jpa annotation

--- src/main/java/spe_booker/SpeBookingApplication.java
+++ src/main/java/spe_booker/SpeBookingApplication.java
@@ -12,7 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 })
 
 
-@EnableJpaRepositories("spe_booker")
+@EnableJpaRepositories
 @SpringBootApplication
 public class SpeBookingApplication {
 

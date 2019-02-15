1ad51f2fb98ace288f1bd6c958dacd760df90eec
zackdove <zdove@live.co.uk>
2019-02-15 11:11:15 +0000

    Added autowired to userrepository bean

--- src/main/java/spe_booker/Repositorys/UserRepository.java
+++ src/main/java/spe_booker/Repositorys/UserRepository.java
@@ -3,6 +3,7 @@ package spe_booker.Repositorys;
 import spe_booker.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {

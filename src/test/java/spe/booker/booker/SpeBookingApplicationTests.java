ee0c3e3381faf1998f4c28039a475b01b6ae27db
Unknown <aidanhood@btinternet.com>
2019-02-15 10:35:46 +0000

    d

--- src/test/java/spe/booker/booker/SpeBookingApplicationTests.java
+++ src/test/java/spe/booker/booker/SpeBookingApplicationTests.java
@@ -5,9 +5,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import spe_booker.SpeBookingApplication;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class)
+@SpringBootTest(classes = SpeBookingApplication.class)
 public class SpeBookingApplicationTests {
 
     @Test

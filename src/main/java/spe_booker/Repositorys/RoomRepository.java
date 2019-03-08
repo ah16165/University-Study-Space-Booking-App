f8df1def0107e250b10faa8cc792a8ff94a6ef73
Unknown <aidanhood@btinternet.com>
2019-03-08 13:25:44 +0000

    added room service

--- src/main/java/spe_booker/Repositorys/RoomRepository.java
+++ src/main/java/spe_booker/Repositorys/RoomRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.springframework.stereotype.Repository;
 import spe_booker.models.Room;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @Repository
 public interface RoomRepository extends CrudRepository<Room, Long> {
---
+++ src/main/java/spe_booker/models/RoomService.java
@@ -0,0 +1,51 @@
+package spe_booker.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import spe_booker.Repositorys.RoomRepository;
+import spe_booker.Repositorys.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class RoomService {
+
+
+    private RoomRepository roomRepository;
+
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public RoomService(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+
+    }
+
+
+    public Room findById(Long id) {
+        return roomRepository.findById(id).get();
+    }
+
+    public Room createRoom(Long id, String roomNo, String building, Integer capacity, List<Booking> bookings) {
+        Room s = new Room();
+
+        s.setId(id);
+        s.setRoomNo(roomNo);
+        s.setBuilding(building);
+        s.setCapacity(capacity);
+        s.setBookings(bookings);
+        saveRoom(s);
+
+        return s;
+    }
+
+    public Room saveRoom(Room room) {
+        return roomRepository.save(room);
+    }
+
+
+
+}

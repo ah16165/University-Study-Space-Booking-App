41621ddf4b343899ce5245daa737094fb80abd54
zackdove <zdove@live.co.uk>
2019-03-13 14:21:52 +0000

    Added a find by room and building query

--- src/main/java/spe_booker/Repositorys/RoomRepository.java
+++ src/main/java/spe_booker/Repositorys/RoomRepository.java
@@ -9,4 +9,5 @@ import spe_booker.models.Room;
 @Repository
 public interface RoomRepository extends CrudRepository<Room, Long> {
     List<Room> findAll();
+    Room findByRoomNoAndBuilding(String roomNo, String building);
 }

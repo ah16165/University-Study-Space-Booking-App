c6a67a0b2aa973f5ec10bb0c642f198b701c940d
zackdove <zdove@live.co.uk>
2019-03-19 14:45:41 +0000

    Add error logging

--- src/main/java/spe_booker/Controllers/CustomErrorController.java
+++ src/main/java/spe_booker/Controllers/CustomErrorController.java
@@ -1,5 +1,7 @@
 package spe_booker.Controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -11,6 +13,8 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class CustomErrorController implements ErrorController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(BookingController.class);
+
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
@@ -20,14 +24,19 @@ public class CustomErrorController implements ErrorController {
 
             switch (statusCode) {
                 case 400:
+                    LOG.info("400: Bad Request Error\n");
                     return "error/error-400";
                 case 403:
-                  return "error/error-403";
+                    LOG.info("403: Forbidden Error\n");
+                    return "error/error-403";
                 case 404:
+                    LOG.info("404: Page Not Found Error\n");
                     return "error/error-404";
                 case 500:
+                    LOG.info("500: Internal Server Error\n");
                     return "error/error-500";
                 default:
+                    LOG.info(statusCode + ": Error\n");
                     return "error/error";
             }
         }

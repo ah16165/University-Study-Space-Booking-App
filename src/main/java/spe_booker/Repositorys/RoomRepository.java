efdc769b2d7b0e760dcf5a00f46261527f98abb7
Unknown <at17556@my.bristol.ac.uk>
2019-03-20 03:25:09 +0000

    attempted jpa query zack asked for - cant test on my machine as program doesnt compile atm

--- src/main/java/spe_booker/Repositorys/RoomRepository.java
+++ src/main/java/spe_booker/Repositorys/RoomRepository.java
@@ -1,14 +1,24 @@
 package spe_booker.Repositorys;
 
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
+import spe_booker.models.Booking;
 import spe_booker.models.Room;
 
 @Repository
 public interface RoomRepository extends CrudRepository<Room, Long> {
+    @Query("select r from room r inner join booking b on r.id = b.id where b.date_time = :#{#booking.dateTime} and b.length = :#{#booking.length}")
+    List<Room> findByDateTimeLength(@Param("booking") Booking booking);
+
+    //where b.date_time = :dateTime and b.length = :length)
+    //List<Room> findByDateTimeLength(@Param("dateTime") Date dateTime,
+    //                             @Param("length") Long length);
+}
     List<Room> findAll();
     Optional<Room> findByRoomNoAndBuilding(String roomNo, String building);
 }

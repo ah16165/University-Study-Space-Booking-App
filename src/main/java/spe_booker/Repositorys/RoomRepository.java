1ceaecfe16bb0663ed4d935ed4e38d629e6e0033
Unknown <aidanhood@btinternet.com>
2019-03-22 11:31:25 +0000

    Incomplete, but better SQL query

--- src/main/java/spe_booker/Repositorys/RoomRepository.java
+++ src/main/java/spe_booker/Repositorys/RoomRepository.java
@@ -14,11 +14,15 @@ import spe_booker.models.Room;
 
 @Repository
 public interface RoomRepository extends CrudRepository<Room, Long> {
-//    @Query("select r from room r inner join booking b on r.id = b.id where b.date_time = :#{#booking.getDateTime()} and b.length = :#{#booking.getLength()}")
-//    List<Room> findByDateTimeLength(@Param("booking") Booking booking);
-//
-//    SELECT roomid FROM rooms where
+
+    //@Query("select r from rooms r where not exists (select 1 from bookings b where b.room_id = r.room_id and @input_date_time < date_add(b.date_time, interval length 1)) and date_add(@input_date_time, interval @input_length 1) > b.date_time")
+    //List<Room> findByDateTimeLength(@Param("input_date_time") Date date, @Param("input_length") Long length);
 
     List<Room> findAll();
     Optional<Room> findByRoomNoAndBuilding(String roomNo, String building);
 }
+
+
+
+
+
